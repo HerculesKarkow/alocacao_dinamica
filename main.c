9439bab8d4a6c251b2d9ca7cd777d6d62ff618c7
Hercules Karkow <heco_karkow98@hotmail.com>
2016-09-16 22:07:28 -0300

    programa funcional - realloc

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <malloc.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -27,6 +28,25 @@ int main(int argc, char *argv[]) {
 	}
 	
 	//exibe o conteudo da memoria alocada
+	printf("O conteudo da memoria alocada eh: %s\n", memoria_alocada);
+	
+	// altera a quantidade de memoria alocada para (100 x char)
+	memoria_alocada = realloc(memoria_alocada, 100 * sizeof(char));
+	
+	//se o ponteiro foi inicializado com o valor nulo
+	if(memoria_alocada == NULL)
+	{
+		// exibe a mensagem de erro
+		printf("Nao foi possivel alocar a memoria alocada!\n");
+	}
+	// se o ponteiro nao for nulo
+	else 
+	{
+		//transfere a mensagem para a memoria alocada
+		strcpy(memoria_alocada, "Quantidade de memoria alocada alterada!\n");
+	}
+	
+	// exibe o conteudo da memoria alocada
 	printf("O conteudo da memoria alocada eh: %s\n", memoria_alocada);
 	
 	//libera a memoria alocada
